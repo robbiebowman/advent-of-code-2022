dc317314cf6ec2d69fcb1e81813adecea89bcfa9
Robbie Bowman <robbiebowman@gmail.com>
2022-12-06 21:00:00 -0500

    Fix communicator device to receive unintelligible strings

---
+++ day-6/Program.fs
@@ -0,0 +1,18 @@
+﻿// Preamble
+let readLines filePath = System.IO.File.ReadLines(filePath)
+
+let line =
+    Seq.toList (readLines "input.txt")
+    |> List.head
+    |> Seq.toList
+
+// Part 1
+let allUnique cs = Set cs |> Set.count = (Seq.length cs)
+
+let uniqueCharsIndex windowSize (cs: char list) =
+    windowSize + Seq.findIndex (fun i -> allUnique cs[i..(i + windowSize - 1)]) [0..(List.length cs)]
+
+printfn "%i" (uniqueCharsIndex 4 line) // 1140
+
+// Part 2
+printfn "%i" (uniqueCharsIndex 14 line) // 3495
